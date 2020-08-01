237c99c669d716c982ece2d354ceb07401fd2057
shubhendu23 <64519002+shubhendu23@users.noreply.github.com>
2020-08-01 15:44:57 +0530

    1st

---
+++ task6.groovy
@@ -0,0 +1,70 @@
+freeStyleJob('deployment launch') {
+     scm {
+        git {
+            remote {
+                name('origin')
+                url('https://github.com/shubhendu23/task6web.git')
+            }
+        }
+    triggers {
+        
+    }
+    steps {
+       
+        shell ('kubectl create -f /root/task6kube/pvc.yml ')
+        shell (' sleep 60 ')
+        shell ('if [[ $(ls | grep php) ]] ; then kubectl create -f /root/task6kube/deploymentphp.yml; else kubectl create -f /root/task6kube/deployment.yml; fi ')
+        shell (' sleep 60 ')
+        shell ('bash /root/task6kube/copypage.sh')
+        shell ('kubectl create -f /root/task6kube/service.yml ')
+        }
+        
+    }
+    
+}
+freeStyleJob('testing') {
+    
+    triggers {
+        upstream('deployment launch', 'SUCCESS')
+    }
+    steps {
+        
+        shell ('status=$(curl -o /dev/null -s -w %{http_code} 192.168.99.100:30000)')
+        shell ('if [[ $(curl -o /dev/null -s -w %{http_code} 192.168.99.100:30000) == 200 ]]; then exit 0; else exit 1; fi ')
+    }
+     publishers{
+        downstreamParameterized {
+            trigger( 'unstable notify') {
+                condition( 'FAILED')
+                triggerWithNoParameters(triggerWithNoParameters = true)
+
+            }
+        }
+    }
+    
+}
+    
+
+freeStyleJob('unstable notify') {
+    
+   /* triggers {
+         upstream('testing', 'ABORTED')   
+    }*/
+    steps {
+      shell ('python3 /root/task6kube/mail.py')
+        
+        
+    }
+    
+}
+buildPipelineView('task6') {
+    filterBuildQueue()
+    filterExecutors()
+    title('task6')
+    displayedBuilds(5)
+    selectedJob('deployment launch')
+    alwaysAllowManualTrigger()
+    showPipelineParameters()
+    refreshFrequency(60)
+}
+//shubhenduSaurabh  
